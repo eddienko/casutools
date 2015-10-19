d4cd2f8d459988532e59dc943217304123626a9a
Eduardo Gonzalez <eglez@ast.cam.ac.uk>
2015-10-19 11:44:19 +0100

    Added initial skeleton

---
+++ casutools/background.py
@@ -0,0 +1,11 @@
+import numpy as np
+from astropy.stats import sigma_clip
+from astropy.utils import lazyproperty
+import warnings
+
+import astropy
+
+__all__ = ['Background']
+
+class Background(object):
+    pass
